737d365aa3a50ac5050020196ced5f1116c63490
AleksandrIstrelov <istrelov004@gmail.com>
2022-12-18 12:52:58 +0300

    add example arraymetod

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 35, 4, 56, 69, 22, -55, 35};
+
+int n = array.Length;
+int find = 35;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
