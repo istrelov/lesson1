23b3c9c2166b20637111f74b1a64c94530224a36
AleksandrIstrelov <istrelov004@gmail.com>
2022-12-18 13:14:58 +0300

    add ezmple array library

---
+++ Example012_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collect)
+{
+    int length = collect.Length;
+    int index = 0;
+
+    while(index < length)
+    {   
+        collect[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int count = collection.Length;
+    int posit = 0;
+
+    while(posit < count)
+    {
+        Console.WriteLine(collection[posit]);
+        posit++;
+    }
+}
+
+int IndexOf(int[] CollectArray, int find)
+{
+    int count = CollectArray.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(CollectArray[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 12);
+Console.WriteLine(pos);
